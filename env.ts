4ed7e75a8bac6bbb1f49fa538e2e01ebac326e4d
dmostafiz <dev.mostafiz@gmail.com>
2023-01-25 00:01:15 +0600

    env.ts changed

--- env.ts
+++ env.ts
@@ -15,14 +15,14 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
+	HOST: Env.schema.string.optional({ format: 'host' }),
+	PORT: Env.schema.number.optional(),
 	APP_KEY: Env.schema.string(),
 	APP_NAME: Env.schema.string(),
 
 	DRIVE_DISK: Env.schema.enum(['local', 's3'] as const),
 
-	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+	NODE_ENV: Env.schema.enum.optional(['development', 'production', 'test'] as const),
 
 	SMTP_HOST: Env.schema.string({ format: 'host' }),
 	SMTP_PORT: Env.schema.number(),
